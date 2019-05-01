2d6c9c62bd12558515c48f4e61db2a8acc81f338
Ignasi Perez <perezignasi@gmail.com>
2019-05-01 16:44:51 +0200

    Days 16_17 - Projects 01_01 - C01A-StormViewer

---
+++ D16_18-P01_01-C01A-StormViewer/C01A-StormViewer/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  C01A-StormViewer
+//
+//  Created by Ignasi Perez-Valls on 01/05/2019.
+//  Copyright © 2019 ignasiSwift. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
