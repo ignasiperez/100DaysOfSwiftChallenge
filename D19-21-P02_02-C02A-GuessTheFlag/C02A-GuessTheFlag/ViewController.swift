c61b9e990c4c82101c2e7daf3646021c517e05b7
Ignasi Perez <perezignasi@gmail.com>
2019-05-04 17:34:02 +0200

    Day(s) 19_20 – Project(s) 02_02 - C02A-GuessTheFlag

--- D19-21-P02_02-C02A-GuessTheFlag/C02A-GuessTheFlag/ViewController.swift
+++ D19-21-P02_02-C02A-GuessTheFlag/C02A-GuessTheFlag/ViewController.swift
@@ -11,7 +11,15 @@ import UIKit
 class ViewController: UIViewController {
     
     //  ************************************************************
-    //  MARK: - @IBOutlet
+    //  MARK: - Instance properties
+    //
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    
+    
+    //  ************************************************************
+    //  MARK: - @IBOutlet Instance Properties
     //
     @IBOutlet var button1: UIButton!
     @IBOutlet var button2: UIButton!
@@ -19,14 +27,7 @@ class ViewController: UIViewController {
     
     
     //  ************************************************************
-    //  MARK: - Instance properties
-    //
-    var countries = [String]()
-    var score = 0
-    
-    
-    //  ************************************************************
-    //  MARK: - Overrided methods UIViewController
+    //  MARK: - Overrided Methods UIViewController
     //
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,13 +50,48 @@ class ViewController: UIViewController {
     }
     
     
+    //  *****************************************************************
+    //  MARK: - @IBAction Instance Methods
+    //
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        
+        let ac = UIAlertController(title: title,
+                                   message: "Your score is \(score)",
+                                   preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(
+            title: "Continue",
+            style: .default,
+            handler: askQuestion))
+
+        present(ac, animated: true)
+    }
+    
+    
     //  ************************************************************
     //  MARK: - Instance methods
     //
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil ) {
+        countries.shuffle()
+        
+        // static func Int.random(in range: Range<Int>) -> Int
+        correctAnswer = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
     }
     
 }
