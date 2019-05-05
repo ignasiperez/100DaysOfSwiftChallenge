89b519307e3ef8d320df3906c0b7a4e5d9312a8d
Ignasi Perez <perezignasi@gmail.com>
2019-05-05 23:16:00 +0200

    Day(s) 22_22 – Project(s) 03_03 - C03A-SocialMedia

---
+++ D22_22-P01_01-C03A-SocialMedia/C03A-SocialMedia/DetailViewController.swift
@@ -0,0 +1,96 @@
+//
+//  DetailViewController.swift
+//  C01A-StormViewer
+//
+//  Created by Ignasi Perez-Valls on 01/05/2019.
+//  Copyright © 2019 ignasiSwift. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    //  ************************************************************
+    //  MARK: - Instance properties
+    //
+    var selectedImge: String?
+    
+    var selectedPicturePosition = 0
+    var totalPictures = 0
+    
+    
+    //  ************************************************************
+    //  MARK: - @IBOutlet Instance Properties
+    //
+    @IBOutlet var imageView: UIImageView!
+    
+    
+    //  ************************************************************
+    //  MARK: - Overrided methods UIViewController
+    //
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("\nDetailViewController viewDidLoad()")
+        
+        // day 16: title = selectedImage
+        // day 18 (challenge): 
+        title = "Picture \(selectedPicturePosition) of \(totalPictures)"
+        
+        // enum UINavigationItem.LargeTitleDisplayMode: Int
+        // Enumeration Cases
+        //  .automatic, .always, .never
+        navigationItem.largeTitleDisplayMode = .never
+        
+        
+        // enum UIBarButtonItem.SystemItem: Int
+        // Constants
+        //  .action, .add,  .refresh, .search, .camera, .play...
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        
+        
+        
+        
+        
+        // Do any additional setup after loading the view.
+        if let imageToLoad = selectedImge {
+            imageView.image  = UIImage(named: imageToLoad)
+            print("- imageToLoad: \(imageToLoad)")
+        }
+    }
+    
+    
+    /// Notifies the view controller that its view is about to be added to a view hierarchy.
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    
+    /// Notifies the view controller that its view is about to be removed from a view hierarchy.
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    
+    //  ************************************************************
+    //  MARK: - Instance methods
+    //
+    @objc func shareTapped() {
+        guard let image =
+            imageView.image?.jpegData(compressionQuality: 0.8) else {
+                print("No image found")
+                return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image],
+                                          applicationActivities: [])
+        
+        // popoverPresentationController: Instance property of parent class UIViewController
+        vc.popoverPresentationController?.barButtonItem =
+            navigationItem.rightBarButtonItem
+        
+        present(vc, animated: true)
+    }
+
+}
---
+++ D22_22-P01_01-C03A-SocialMedia/C03A-SocialMedia/ViewController.swift
@@ -0,0 +1,97 @@
+//
+//  ViewController.swift
+//  C03A-SocialMedia
+//
+//  Created by Ignasi Perez-Valls on 01/05/2019.
+//  Copyright © 2019 ignasiSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    
+    
+    //  ************************************************************
+    //  MARK: - Overrided methods UIViewController
+    //
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        print("ViewController viewDidLoad()")
+        
+        title = "Storm Viewer"
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            print("- item: \(item)")
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        pictures.sort()
+        
+        print("\n- pictures: \(pictures)")
+    }
+    
+    
+    //  ************************************************************
+    //  MARK: - TABLE Protocols
+    //
+    //  ViewController inherits UITableViewController which conforms
+    //  the protocols UITableViewDataSource and UITableViewDelegate.
+    //
+    //
+    //  Protocol UITableViewDataSource
+    //
+    /// Tells the data source to return the number of rows in a given section of a table view. Required.
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    
+    //  Protocol UITableViewDataSource
+    //
+    /// Asks the data source for a cell to insert in a particular location of the table view. Requiered.
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture",
+                                                 for: indexPath)
+        
+        cell.textLabel?.text = pictures[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    //  Protocol UITableViewDelegate
+    //
+    ///  Tells the delegate that the specified row is now selected.
+    override func tableView(_ tableView: UITableView,
+                            didSelectRowAt indexPath: IndexPath) {
+        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+        if let vc = storyboard?.instantiateViewController(
+            withIdentifier: "Detail") as? DetailViewController {
+            // 2: success! Set its selectedImage property
+            vc.selectedImge = pictures[indexPath.row]
+            
+            let x = indexPath.row + 1
+            vc.selectedPicturePosition = x
+            
+            let y = pictures.count
+            vc.totalPictures = y
+            
+            // 3: now push it onto the navigation controller
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+}
