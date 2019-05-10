8dbb72f5182e3e92c552243ab42795c38c39b177
Ignasi Perez <perezignasi@gmail.com>
2019-05-10 18:33:53 +0200

    Day(s) 24_25 – Project(s) 04_04 - C04A-EasyBrowser

--- D24_26-P04_04-C04A-EasyBrowser/C04A-EasyBrowser/ViewController.swift
+++ D24_26-P04_04-C04A-EasyBrowser/C04A-EasyBrowser/ViewController.swift
@@ -15,12 +15,15 @@ class ViewController: UIViewController, WKNavigationDelegate {
     //  MARK: - Instance properties
     //
     var webView: WKWebView!
+    var progressView: UIProgressView!
+    
+    var websites = ["apple.com", "hackingwithswift.com"]
     
     
     //  ************************************************************
     //  MARK: - Override UIViewController methods
     //
-    //  3. DOWNLOAD A WEB PAGE
+    //  D24-04. DOWNLOAD A WEB PAGE
     override func loadView() {
         print("ViewController loadDidLoad()")
         webView = WKWebView()
@@ -34,8 +37,17 @@ class ViewController: UIViewController, WKNavigationDelegate {
         // Do any additional setup after loading the view.
         print("\nViewController viewDidLoad()")
         
-        // 4. CHOOSE THE WEBSITE
-        //    - Add a button to the navigation bar
+        // D25-2. ADD A `UIProgressView` TO OUR TOOLBAR TO SHOW HOW FAR THE PAGE IS THROUGH LOADING
+        //  - Register as an observer
+        webView.addObserver(
+            self,
+            forKeyPath: #keyPath(WKWebView.estimatedProgress),
+            options: .new,
+            context: nil)
+
+
+        // D24-4. CHOOSE THE WEBSITE
+        //  - Add a button to the navigation bar
         navigationItem.rightBarButtonItem = UIBarButtonItem(
             title: "Open…",
             style: .plain,
@@ -43,40 +55,124 @@ class ViewController: UIViewController, WKNavigationDelegate {
             action: #selector(openTapped)
         )
         
-        // 3. DOWNLOAD A WEB PAGE
-        let url = URL(string: "https://www.hackingwithswift.com")!
-        webView.load(URLRequest(url: url))
+        // D25-02. ADD A `UIProgressView` TO OUR TOOLBAR TO SHOW HOW FAR THE PAGE IS THROUGH LOADING
+        progressView = UIProgressView(
+            progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(
+            customView: progressView)
+
+        
+        // D25-01. SHOW A TOOLBAR AT THE BOTTOM OF THE SCREEN WITH A REFRESH BUTTON ON THE RIGHT SIDE.
+        let spacer = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: nil, action: nil)
+        
+        let refresh = UIBarButtonItem(
+            barButtonSystemItem: .refresh,
+            target: webView,
+            action: #selector(webView.reload))
+        
+        toolbarItems = [progressButton, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+
+        
+        // D24-3. DOWNLOAD A WEB PAGE
+        // let url = URL(string: "https://www.hackingwithswift.com")!
+        // webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+        
+        
+        // D25-3. NAVIGATE TO A WEB SITE ONLY IF ITS URL IS IN OUR SAFE LIST
+        // - Modify the web view's initial web page so that it's not hard-coded.
+        let url = URL(string: "https://" + websites[0])!
+        webView.load(URLRequest(url: url))
+
     }
     
     
     //  ************************************************************
     //  MARK: - Implementation WKNavigationDelegate Protocol methods
     //
-    // 5. SETTING THE TITLE IN THE NAVIGATION BAR WHEN THE PAGE FINISH DOWNLOADING
+    // D24-05. SETTING THE TITLE IN THE NAVIGATION BAR WHEN THE PAGE FINISH DOWNLOADING
     func webView(_ webView: WKWebView,
                  didFinish navigation: WKNavigation!) {
         title = webView.title
     }
     
+    
+    // D25-3. NAVIGATE TO A WEB SITE ONLY IF ITS URL IS IN OUR SAFE LIST
+    //  - Evaluate the URL to see whether it's in our safe list
+    func webView(_ webView: WKWebView,
+                 decidePolicyFor navigationAction: WKNavigationAction,
+                 decisionHandler: @escaping (WKNavigationActionPolicy)
+        -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        
+        decisionHandler(.cancel)
+    }
+    
+    
+    
+    
+    func webView(_ webView: WKWebView,
+                 decidePolicyFor navigationAction: WKNavigationAction,
+                 decisionHandler: @escaping (WKNavigationActionPolicy)
+        -> Void) {
+        
+    }
+
+    
+    
+    //  ************************************************************
+    //  MARK: - Implementation OBSERVER Protocol methods
+    //
+    // D24-05. SETTING THE TITLE IN THE NAVIGATION BAR WHEN THE PAGE FINISH DOWNLOADING
+    override func observeValue(forKeyPath keyPath: String?,
+                               of object: Any?,
+                               change: [NSKeyValueChangeKey : Any]?,
+                               context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+
+    
 
     //  ************************************************************
     //  MARK: - Instance methods
     //
-    // 4. CHOOSE THE WEBSITE
-    //    - When user taps the `Open...` button, iOS engine shows a   message with different websites to choose.
+    // D24-04. CHOOSE THE WEBSITE
+    //  - When user taps the `Open...` button, iOS engine shows a   message with different websites to choose.
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open page…",
                                    message: nil,
                                    preferredStyle: .actionSheet)
         
-        ac.addAction(UIAlertAction(title: "apple.com",
-                                   style: .default,
-                                   handler: openPage))
+        //ac.addAction(UIAlertAction(title: "apple.com",
+        //                           style: .default,
+        //                           handler: openPage))
         
-        ac.addAction(UIAlertAction(title: "hackingwithswift.com",
-                                   style: .default,
-                                   handler: openPage))
+        //ac.addAction(UIAlertAction(title: "hackingwithswift.com",
+        //                           style: .default,
+        //                           handler: openPage))
+        
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website,
+                                       style: .default,
+                                       handler: openPage)
+            )
+        }
+
         
         ac.addAction(UIAlertAction(title: "Cancel",
                                    style: .cancel))
@@ -87,7 +183,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
         present(ac, animated: true)
     }
     
-    // 4. CHOOSE THE WEBSITE
+    // D24-04. CHOOSE THE WEBSITE
     //  - When user chooses one website in the alert action, iOS engine calls this method.
     func openPage(action: UIAlertAction) {
         let url = URL(string: "https://" + action.title!)!
--- D24_26-P04_04-C04A-EasyBrowser/README.md
+++ D24_26-P04_04-C04A-EasyBrowser/README.md
@@ -2,7 +2,7 @@
 
 Functional Web browser embedded in the App.
 
-## INSTRUCTIONS [Day 24](https://www.hackingwithswift.com/100/24)
+## INSTRUCTIONS [Hacking with Swift Day 24](https://www.hackingwithswift.com/100/24)
 
 1. SETTING UP
    - Create a new Xcode project
@@ -32,7 +32,7 @@ Functional Web browser embedded in the App.
    - _`ViewController.swift`_ _`openPage(action: UIAlertAction)`_
      - When user chooses one website in the alert action, iOS engine calls to _`openPage(action: UIAlertAction)`_ instance method. (Code 4)
 5. SETTING THE TITLE IN THE NAVIGATION BAR WHEN THE PAGE FINISH DOWNLOADING
-   - _`ViewController.swift`_ _`webView(..., didFinish navigation: WKNavigation)`_
+   - _`ViewController.swift`_ `webView(..., didFinish navigation: WKNavigation)`
      - (Code 5) 
 
 Code 1
@@ -97,5 +97,120 @@ Code 5
 func webView(_ webView: WKWebView,
              didFinish navigation: WKNavigation!) {
   title = webView.·····
+}
+```
+
+## INSTRUCTIONS [Hacking with Swift Day 25](https://www.hackingwithswift.com/100/25)
+
+1. SHOW A TOOLBAR AT THE BOTTOM OF THE SCREEN WITH A REFRESH BUTTON ON THE RIGHT SIDE.
+   - _`ViewController.swift`_ `viewDidLoad()`
+     - (Code 1)
+2. ADD A `UIProgressView` TO OUR TOOLBAR TO SHOW HOW FAR THE PAGE IS THROUGH LOADING
+   - _`ViewController.swift`_
+     - Declare the _`progressView`_ property of type `UIProgressView`.
+   - _`ViewController.swift`_ `viewDidLoad()`  
+     - Create a new instance with the default style (Code 2)
+     - Add to the _`toolbarItems`_ array the new progress view (Code 3)
+     - Register as an observer (Code 4)
+     - Listening when an observed value has changed (Code 5)
+3. NAVIGATE TO A WEB SITE ONLY IF ITS URL IS IN OUR SAFE LIST
+   - _`ViewController.swift`_
+     - Refactor our code so there's a shared array of allowed websites (Code 6)
+   - _`ViewController.swift`_ `viewDidLoad()`
+     - Modify the web view's initial web page so that it's not hard-coded (Code 7)
+   - _`ViewController.swift`_`webView(..., decidePolicyFor navigationAction: WKNavigationAction, ...)`
+     - Evaluate the URL to see whether it's in our safe list (Code 8)
+ 
+   Code 1
+
+```swift
+let spacer = UIBarButtonItem(
+    barButtonSystemItem: .flex·····, 
+    target: ·····,
+    action: ·····)
+
+let refresh = UIBarButtonItem(
+    barButtonSystemItem: .re·····, 
+    target: w·····, 
+    action: #selector(webView.·····))
+
+toolbarItems = [spacer, refresh]
+navigationController?.isToolbarHidden = false
+```
+
+Code 2
+
+```swift
+progressView = UIProgressView(
+      progressViewStyle: .····)
+      progressView.size·····()
+let progressButton = UIBarButtonItem(
+      customView: ·····)
+```
+
+Code 3
+
+```swift
+toolbarItems = [progressButton, spacer, refresh]
+```
+
+Code 4
+
+```swift
+ webView.addObserver(
+            self,
+            forKeyPath: #keyPath(WKWebView.·····),
+            options: .·····,
+            context: ·····)
+```
+
+Code 5
+
+```swift
+override func observeValue(forKeyPath keyPath: String?,
+                            of object: Any?,
+                            change: [NSKeyValueChangeKey : Any]?,
+                            context: UnsafeMutableRawPointer?) {
+  if keyPath == "esti·····" {
+    progressView.progress = ·····(webView.estimatedProgress)
+  }
+}
+```
+
+Code 6
+
+```swift
+var websites = ["apple.com", "hackingwithswift.com"]
+```
+
+Code 7
+
+```swift
+for website in websites {
+  ac.addAction(UIAlertAction(title: website,
+                              style: .·····,
+                              handler: openPage)
+  )
+}
+
+Code 8
+
+```swift
+func webView(_ webView: WKWebView,
+                 decidePolicyFor navigationAction: WKNavigationAction,
+                 decisionHandler: @escaping (WKNavigationActionPolicy)
+        -> Void) {
+  let url = navigationAction.request.·····
+        
+  if let host = url?.host {
+    for website in websites {
+      if host.·····(website) {
+        decisionHandler(.·····)
+          return
+      }
+    }
+  }
+        
+  decisionHandler(.·····)
 }
 ```
